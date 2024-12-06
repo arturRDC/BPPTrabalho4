133d99baabba2b0c2d3f88333f7e1061c9a0cf69
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-12-06 11:04:26 -0300

    Adicionar Produto

---
+++ src/main/java/br/ufrn/imd/modelo/Produto.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.modelo;
+
+public class Produto {
+    public int id;
+    public String nome;
+    public float preco;
+
+    public Produto(int id, String nome, float preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
